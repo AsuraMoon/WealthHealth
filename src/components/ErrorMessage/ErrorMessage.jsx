9d1cb281d89ea8f84a4103ca988aa65d8ab84968
AsuraMoon <73102506+AsuraMoon@users.noreply.github.com>
2023-12-11 10:00:10 +0100

    Verif form + Modal error form
    Verification Modal + Preparation Module NPM pour modal error

---
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -0,0 +1,5 @@
+const ErrorMessage = ({ message }) => {
+  return <div className="error-message">{message}</div>;
+};
+
+export default ErrorMessage;
--- src/components/Form/PersonForm.jsx
+++ src/components/Form/PersonForm.jsx
@@ -1,11 +1,14 @@
+// Importation des modules nécessaires depuis React et d'autres bibliothèques
 import { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { usePersonContext } from '../../store/PersonContext';
 import { states } from '../../assets/data/state';
-import { departmentOptions} from '../../assets/data/departmentOptions';
-import './PersonForm.scss';
+import { departmentOptions } from '../../assets/data/departmentOptions';
+import ErrorMessage from '../ErrorMessage/ErrorMessage'; // Import du composant ErrorMessage
+import './PersonForm.scss'; // Import du fichier de styles
 
+// Définition du composant PersonForm
 const PersonForm = () => {
   // Utilisation du contexte pour accéder aux fonctions d'ajout de personne
   const { addPerson } = usePersonContext();
@@ -21,9 +24,40 @@ const PersonForm = () => {
   const [zipCode, setZipCode] = useState('');
   const [department, setDepartment] = useState('');
 
+  // États locaux pour gérer les messages d'erreur
+  const [firstNameError, setFirstNameError] = useState('');
+  const [lastNameError, setLastNameError] = useState('');
+  const [zipCodeError, setZipCodeError] = useState('');
+
   // Fonction de gestion de la soumission du formulaire
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    // Vérification des champs obligatoires
+    if (!firstName) {
+      setFirstNameError('Veuillez saisir le prénom.');
+      return;
+    } else {
+      setFirstNameError('');
+    }
+
+    if (!lastName) {
+      setLastNameError('Veuillez saisir le nom de famille.');
+      return;
+    } else {
+      setLastNameError('');
+    }
+
+    // ... (ajoutez des vérifications similaires pour d'autres champs)
+
+    // Vérification du format de code postal
+    const zipCodePattern = /^\d{5}$/;
+    if (!zipCodePattern.test(zipCode)) {
+      setZipCodeError('Le code postal doit contenir exactement 5 chiffres.');
+      return;
+    } else {
+      setZipCodeError('');
+    }
 
     // Création d'un objet représentant une personne avec les données du formulaire
     const newPerson = {
@@ -53,47 +87,50 @@ const PersonForm = () => {
     setDepartment('');
   };
 
+  // Rendu du composant
   return (
     <form onSubmit={handleSubmit} className='WrapForm'>
       {/* Champ du prénom */}
       <label>
-        First Name:
+        Prénom :
       </label>
       <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+      <ErrorMessage message={firstNameError} />
 
       {/* Champ du nom de famille */}
       <label>
-        Last Name:
+        Nom de famille :
       </label>
       <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+      <ErrorMessage message={lastNameError} />
 
       {/* Champ de la date de naissance avec le sélecteur de date */}
       <label>
-        Date of Birth:
+        Date de naissance :
       </label>
       <DatePicker selected={dob} onChange={(date) => setDob(date)} />
 
       {/* Champ de la date de début avec le sélecteur de date */}
       <label>
-        Start Date:
+        Date de début :
       </label>
       <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
 
       {/* Champ de la rue */}
       <label>
-        Street:
+        Rue :
       </label>
       <input type="text" value={street} onChange={(e) => setStreet(e.target.value)} />
 
       {/* Champ de la ville */}
       <label>
-        City:
+        Ville :
       </label>
       <input type="text" value={city} onChange={(e) => setCity(e.target.value)} />
 
       {/* Champ de l'État avec une liste déroulante */}
       <label>
-        State:
+        État :
       </label>
       <select value={state} onChange={(e) => setState(e.target.value)}>
         <option value="">Sélectionnez un État</option>
@@ -106,13 +143,14 @@ const PersonForm = () => {
 
       {/* Champ du code postal avec une validation pour n'accepter que des chiffres */}
       <label>
-        Zip Code:
+        Code postal :
       </label>
-      <input type="text" pattern="\d*" value={zipCode} onChange={(e) => setZipCode(e.target.value)}/>
+      <input type="text" pattern="\d*" value={zipCode} onChange={(e) => setZipCode(e.target.value)} />
+      <ErrorMessage message={zipCodeError} />
 
       {/* Champ du département avec une liste déroulante */}
       <label>
-        Department:
+        Département :
       </label>
       <select value={department} onChange={(e) => setDepartment(e.target.value)}>
         <option value="">Sélectionnez un département</option>
@@ -124,9 +162,12 @@ const PersonForm = () => {
       </select>
 
       {/* Bouton de soumission du formulaire */}
-      <button type="submit" className='SubmitButton'>Ajouter la personne</button>
+      <button type="submit" className='SubmitButton'>
+        Ajouter la personne
+      </button>
     </form>
   );
 };
 
+// Exportation du composant PersonForm
 export default PersonForm;
