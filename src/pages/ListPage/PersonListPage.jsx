7194746a5b0f7375d3b06a3b305341b0a2eeb018
AsuraMoon <73102506+AsuraMoon@users.noreply.github.com>
2024-01-10 11:18:03 +0100

    Save Before Npm

--- src/pages/ListPage/PersonListPage.jsx
+++ src/pages/ListPage/PersonListPage.jsx
@@ -4,9 +4,6 @@ import PersonList from '../../components/PersonList/PersonList';
 const PersonListPage = () => {
   return (
     <div>
-      {/* En-tête de la page */}
-      <h2>Liste des Employés</h2>
-
       {/* Contenu de la page, incluant le composant PersonList */}
       <div>
         <PersonList />
