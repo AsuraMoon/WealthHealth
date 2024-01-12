16d133ec66ce397172d36a49e4aabd021b5d48da
AsuraMoon <73102506+AsuraMoon@users.noreply.github.com>
2024-01-12 10:16:58 +0100

    Save post Pop

--- src/components/ConfirmationMessage/ConfirmationMessage.jsx
+++ src/components/ConfirmationMessage/ConfirmationMessage.jsx
@@ -1,40 +1,35 @@
+// ConfirmationMessage.jsx
 import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './ConfirmationMessage.scss';
 
-// Définition du composant ConfirmationMessage
 const ConfirmationMessage = ({ isVisible, onHide }) => {
-  // Effet secondaire pour masquer le message après 3 secondes
   useEffect(() => {
-    // Utilisation de setTimeout pour masquer le message après 3 secondes
     const timeoutId = setTimeout(() => {
       onHide();
     }, 3000);
 
-    // Nettoyer le timeout lors du démontage du composant
     return () => clearTimeout(timeoutId);
   }, [isVisible, onHide]);
 
-  // Fonction pour gérer la fermeture immédiate
   const handleClose = () => {
     onHide();
   };
 
-  // Rendu du composant
   return (
-    <div className={`confirmation-message ${isVisible ? 'visible' : 'hidden'}`}>
-      La personne a été enregistrée avec succès.
-      {/* Ajout du bouton de fermeture rapide */}
-      <button onClick={handleClose}>X</button>
+    // Ajoutez la classe 'overlay-visible' lorsque la popup est visible
+    <div className={`confirmation-overlay ${isVisible ? 'overlay-visible' : ''}`}>
+      <div className={`confirmation-message ${isVisible ? 'visible' : 'hidden'}`}>
+        La personne a été enregistrée avec succès.
+        <button onClick={handleClose}>X</button>
+      </div>
     </div>
   );
 };
 
-// Validation des types de props avec PropTypes
 ConfirmationMessage.propTypes = {
-  isVisible: PropTypes.bool.isRequired, // isVisible doit être un booléen requis
-  onHide: PropTypes.func.isRequired, // onHide doit être une fonction requise
+  isVisible: PropTypes.bool.isRequired,
+  onHide: PropTypes.func.isRequired,
 };
 
-// Exportation du composant ConfirmationMessage
 export default ConfirmationMessage;
