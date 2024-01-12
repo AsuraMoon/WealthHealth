8955017546b1a69f02bdbb5d311dc6513fd645f6
AsuraMoon <73102506+AsuraMoon@users.noreply.github.com>
2024-01-12 10:17:43 +0100

    ADD commentaire

--- src/components/ConfirmationMessage/ConfirmationMessage.jsx
+++ src/components/ConfirmationMessage/ConfirmationMessage.jsx
@@ -1,35 +1,47 @@
 // ConfirmationMessage.jsx
-import { useEffect } from 'react';
-import PropTypes from 'prop-types';
-import './ConfirmationMessage.scss';
 
+// Importation des fonctionnalités nécessaires depuis React
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';  // Import PropTypes pour la validation des propriétés
+import './ConfirmationMessage.scss';  // Import du fichier de style SCSS
+
+// Définition du composant ConfirmationMessage
 const ConfirmationMessage = ({ isVisible, onHide }) => {
+  // Effet secondaire pour masquer le message après 3 secondes
   useEffect(() => {
+    // Utilisation de setTimeout pour masquer le message après 3 secondes
     const timeoutId = setTimeout(() => {
       onHide();
     }, 3000);
 
+    // Nettoyer le timeout lors du démontage du composant
     return () => clearTimeout(timeoutId);
   }, [isVisible, onHide]);
 
+  // Fonction pour gérer la fermeture immédiate
   const handleClose = () => {
     onHide();
   };
 
+  // Rendu du composant
   return (
     // Ajoutez la classe 'overlay-visible' lorsque la popup est visible
     <div className={`confirmation-overlay ${isVisible ? 'overlay-visible' : ''}`}>
+      {/* Conteneur du message de confirmation */}
       <div className={`confirmation-message ${isVisible ? 'visible' : 'hidden'}`}>
         La personne a été enregistrée avec succès.
+        {/* Bouton pour fermer la popup */}
         <button onClick={handleClose}>X</button>
       </div>
     </div>
   );
 };
 
+// Validation des types de propriétés avec PropTypes
 ConfirmationMessage.propTypes = {
-  isVisible: PropTypes.bool.isRequired,
-  onHide: PropTypes.func.isRequired,
+  isVisible: PropTypes.bool.isRequired, // isVisible doit être un booléen requis
+  onHide: PropTypes.func.isRequired, // onHide doit être une fonction requise
 };
 
+// Exportation du composant ConfirmationMessage
 export default ConfirmationMessage;
