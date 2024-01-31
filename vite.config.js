8033e565f9b49a24ac217740e4963aa63033f3a7
AsuraMoon <73102506+AsuraMoon@users.noreply.github.com>
2024-01-31 10:11:42 +0100

    END

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import ViteCompressionPlugin from 'vite-plugin-compression';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    ViteCompressionPlugin(),
+  ],
+});
